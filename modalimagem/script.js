b888ea1f9cc6b4e55fd9e824d7bdeccc7002dc4e
THE_L1NX <143143750+SalisSilva337@users.noreply.github.com>
2024-03-22 11:41:19 -0300

    Delete modalimagem directory

--- modalimagem/script.js
+++
@@ -1,9 +0,0 @@
-var img1 = document.querySelector('#img1')
-var modal = document.querySelector ('#modalimg')
-var modalimgg = document.querySelector ('#imgg')
-
-
-function ampliar() {
-    modal.style.display = 'block';
-    modalimgg.src = img1.src;
-}
