73c23d660ce02729bd5dfcf103f29be2d798089e
Salis Silva <salis.silva@aluno.senai.br>
2024-03-25 15:59:15 -0300

    a

--- teste/teste.js
+++ teste/teste.js
@@ -1,9 +1,10 @@
 const playerBotaoContinuar = document.querySelector('#playerBotaoContinuar');
 const playerBotaoPausar = document.querySelector('#playerBotaoPausar');
-const barraProgresso = document.querySelector('#barraProgresso');
+const playerBotaoAnterior = document.querySelector('#playerBotaoAnterior');
+const playerBotaoProximo = document.querySelector('#playerBotaoProximo');
 const musicaInicio = document.querySelector('#musicaInicio');
 const musicaFinal = document.querySelector('#musicaFinal');
-
+const musicasCurtidas = document.querySelector('#musicasCurtidas')
 const musicaPlayer = document.querySelector('#musicaPlayer');
 
 const nomeMusica = document.querySelector('#nomeMusica');
@@ -13,7 +14,11 @@ const limiteMusica = document.querySelector('#limiteMusica');
 const hipHop = ['songs/hiphop/Ja Rule - New York.mp3','songs/hiphop/Fat Joe - Lean Back.mp3','songs/hiphop/Jay-Z - Trouble.mp3','songs/hiphop/Nelly - Ride Wit Me.mp3','songs/hiphop/Eminem feat. Akon - Smack That.mp3','songs/hiphop/Mike Jones - Still Tippin.mp3','songs/hiphop/Busta Rhymes - Pass The Courvoisier Part II.mp3','songs/hiphop/Lil Wayne - Go DJ.mp3'];
 const rock = ['songs/rock/Nirvana - Smells Like Teen Spirit.mp3','songs/rock/Papa Roach - Last Resort.mp3','songs/rock/Red Hot Chili Peppers - Californication.mp3'];
 
-contador = 0; 
+contadorHiphop = 0;
+contadorRock = 0;
+
+
+
 function stopSong(){
     playerBotaoPausar.hidden = true;
     playerBotaoContinuar.hidden = false;
@@ -29,12 +34,11 @@ function continueSong(){
 }
 
 function Hiphop(){
-    playerBotaoPausar.disabled = false;
-    musicaPlayer.src = hipHop[contador = 0];
+    musicaPlayer.src = hipHop[contadorHiphop = 0];
     musicaPlayer.volume = 0.2;
     musicaPlayer.play();
-    nomeMusica.textContent = hipHop[contador].replace('songs/hiphop/','');
-    faixasMusica.textContent = contador + 1;
+    nomeMusica.textContent = hipHop[contadorHiphop].replace('songs/hiphop/','');
+    faixasMusica.textContent = contadorHiphop + 1;
     limiteMusica.textContent = '/ ' + hipHop.length;
     
 }
@@ -60,38 +64,50 @@ barraProgresso.onchange = function(){
 
 
 function Rock(){
-    musicaPlayer.src = rock[contador];
+    musicaPlayer.src = rock[contadorRock = 0];
     musicaPlayer.volume = 0.2;
     musicaPlayer.play();
-    nomeMusica.textContent = rock[contador].replace('songs/rock/','');
-    faixasMusica.textContent = contador + 1;
+    nomeMusica.textContent = rock[contadorRock].replace('songs/rock/','');
+    faixasMusica.textContent = contadorRock + 1;
 }
 
 function nextSong(){
-    contador++;
+    contadorHiphop++;
+    contadorRock++;
     musicaPlayer.volume = 0.2;
-    // if (Hiphop()) {
-        musicaPlayer.src = hipHop[contador];
+    //  if (Hiphop()) {
+        musicaPlayer.src = hipHop[contadorHiphop];
         musicaPlayer.play();
-        nomeMusica.textContent = hipHop[contador].replace('songs/hiphop/','');
-        faixasMusica.textContent = contador + 1;
+        nomeMusica.textContent = hipHop[contadorHiphop].replace('songs/hiphop/','');
+        faixasMusica.textContent = contadorHiphop + 1;
         limiteMusica.textContent = '/ ' + hipHop.length;
 
-    // } if (Rock()) {
-    //     musicaPlayer.src = rock[contador];
-    //     musicaPlayer.play();
-    //     nomeMusica.textContent = rock[contador].replace('songs/rock/','');
-    //     faixasMusica.textContent = contador + 1;
-    // }
+//    } if (Rock()) {
+//         musicaPlayer.src = rock[contadorRock];
+//         musicaPlayer.play();
+//         nomeMusica.textContent = rock[contadorRock].replace('songs/rock/','');
+//         faixasMusica.textContent = ccontadorRock + 1;
+//      }
     
 }
 
 function previousSong(){
-    contador--;
+    contadorHiphop--;
+    contadorRock--;
     musicaPlayer.volume = 0.2;
-    musicaPlayer.src = hipHop[contador];
+    musicaPlayer.src = hipHop[contadorHiphop];
     musicaPlayer.play();
-    nomeMusica.textContent = hipHop[contador].replace('songs/hiphop/','');
-    faixasMusica.textContent = contador + 2 - 1;
+    nomeMusica.textContent = hipHop[contadorHiphop].replace('songs/hiphop/','');
+    faixasMusica.textContent = contadorHiphop + 2 - 1;
     limiteMusica.textContent = '/ ' + hipHop.length;
+}
+
+function musicaCurtida(){
+    let musicasSalvas = document.createElement('tr');
+    musicasSalvas.textContent = hipHop[contadorHiphop].replace('songs/hiphop/','');
+    musicasCurtidas.appendChild(musicasSalvas);
+}
+
+function playList(){
+
 }
