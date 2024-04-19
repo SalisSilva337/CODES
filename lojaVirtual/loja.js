730a28efb815b8ae5a71c5035fc07ae36f81a82c
Salis Silva <salis.silva@aluno.senai.br>
2024-04-19 04:37:23 -0300

    aaaaaaaaa

--- lojaVirtual/loja.js
+++ lojaVirtual/loja.js
@@ -8,8 +8,144 @@ let carrinhoAdd = document.querySelector("#carrinhoAdd");
 window.onload = buscarProdutos();
 
 function buscarProdutos() {
-    
+    mainDiv.innerHTML= ""
     let url = "https://fakestoreapi.com/products/";
+        
+        
+    let request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.send();
+
+    request.onload = function () {
+
+        if (request.status === 200) {
+            console.log(url);
+            let produtos = JSON.parse(request.response);
+            console.log(produtos);
+
+            for (let index = 0; index < produtos.length; index++) {
+                let img = document.createElement("img");
+                img.id = "imagensAPI";
+                img.src = produtos[index].image;
+
+                let imgCarrinho = document.createElement("img");
+                imgCarrinho.id = "imgCarrinhoAPI";
+                imgCarrinho.src = produtos[index].image;
+                
+                
+                
+
+                let h2Modal = document.createElement("h2");
+                h2Modal.id = "h2modal";
+
+                h2Modal.innerHTML = "";
+                
+                
+                let excluir = document.createElement("img");
+                excluir.src = "imgs/excluir.png";
+                excluir.id = "excluirImg";
+
+                let spanFechar = document.createElement("button");
+                let carrinhoAdd = document.createElement("button");
+                
+
+                
+
+                let h2 = document.createElement("h2");
+                h2.textContent = produtos[index].title;
+                h2.id = "nomeProduto";
+                
+                let h2Carrinho = document.createElement("span");
+                h2Carrinho.textContent = produtos[index].title;
+                h2Carrinho.id = "nomeProduto";
+
+                let h2Price = document.createElement("h2");
+                h2Price.textContent ="Preço: $" + produtos[index].price;
+
+                let h2CarrinhoPrice = document.createElement("span");
+                h2CarrinhoPrice.textContent ="Preço: $" + produtos[index].price; 
+
+                let carrinhoItem = document.createElement("div");
+                carrinhoItem.id = "carrinhoItem";
+
+
+                img.addEventListener("click",function (){
+                    
+                    modalConteudo.innerHTML = "";
+
+                    
+                    spanFechar.textContent = "Fechar Modal";
+                    spanFechar.id = "spanFechar";
+                    carrinhoAdd.textContent = "Adicionar ao Carrinho";
+                    carrinhoAdd.id = "carrinhoAdd";
+
+                    modal.style.display = "block";
+                    modalConteudo.style.animation = "modalAnim 0.5s";
+                    h2Modal.textContent = produtos[index].description;
+                    avancarCarrinho.style.display = 'none';
+
+                    modalConteudo.appendChild(spanFechar);
+                    modalConteudo.appendChild(h2Modal);
+                    modalConteudo.appendChild(carrinhoAdd);
+                    modalConteudo.appendChild(spanFechar);
+                    modalConteudo.appendChild(carrinhoAdd);
+                    
+                    img.style.opacity = 0.7;
+                    
+                   
+                    carrinhoAdd.addEventListener("click", function () {
+                        carrinhoAdd.disabled = true;
+                        carrinhoAdd.textContent = "Produto já adicionado";
+
+                        carrinhoItem.appendChild(imgCarrinho);
+                        carrinhoItem.appendChild(h2Carrinho);
+                        carrinhoItem.appendChild(h2CarrinhoPrice);
+                        carrinhoItem.appendChild(excluir);
+                       
+                        carrinhoDiv.appendChild(carrinhoItem);
+                        
+                        
+
+                       
+                    });
+                    
+
+                    
+                });
+
+                spanFechar.addEventListener("click",function (){
+                    modal.style.display = "none";
+                    avancarCarrinho.style.display = 'block';
+                    modalConteudo.removeChild(h2Modal);
+                    img.style.opacity = 1;
+                    
+                }); 
+
+                excluir.addEventListener("click", function () {
+                        carrinhoItem.removeChild(imgCarrinho);
+                        carrinhoItem.removeChild(h2Carrinho);
+                        carrinhoItem.removeChild(h2CarrinhoPrice);
+                        carrinhoItem.removeChild(excluir);
+                       
+                        carrinhoDiv.removeChild(carrinhoItem);
+                });
+
+                
+
+
+                mainDiv.appendChild(img);
+                mainDiv.appendChild(h2);    
+                mainDiv.appendChild(h2Price);
+                
+            }
+        }
+    }
+}
+
+
+function categorias(botao) {
+    mainDiv.innerHTML= ""
+    let url = "https://fakestoreapi.com/products/category/" + botao.value;
         
         
     let request = new XMLHttpRequest();
@@ -43,9 +179,12 @@ function buscarProdutos() {
                 excluir.src = "imgs/excluir.png";
                 excluir.id = "excluirImg";
 
-                let spanFechar = document.createElement("span");
-                let carrinhoAdd = document.createElement("img");
+                let spanFechar = document.createElement("button");
+                let carrinhoAdd = document.createElement("button");
                 
+
+                
+
                 let h2 = document.createElement("h2");
                 h2.textContent = produtos[index].title;
                 h2.id = "nomeProduto";
@@ -69,9 +208,9 @@ function buscarProdutos() {
                     modalConteudo.innerHTML = "";
 
                     
-                    spanFechar.textContent = "X";
+                    spanFechar.textContent = "Fechar Modal";
                     spanFechar.id = "spanFechar";
-                    carrinhoAdd.src = "imgs/carrinho.png";
+                    carrinhoAdd.textContent = "Adicionar ao Carrinho";
                     carrinhoAdd.id = "carrinhoAdd";
 
                     modal.style.display = "block";
@@ -84,11 +223,15 @@ function buscarProdutos() {
                     modalConteudo.appendChild(carrinhoAdd);
                     modalConteudo.appendChild(spanFechar);
                     modalConteudo.appendChild(carrinhoAdd);
-
+                    
                     img.style.opacity = 0.7;
                     
                    
                     carrinhoAdd.addEventListener("click", function () {
+                        carrinhoAdd.disabled = true;
+                        carrinhoAdd.textContent = "Produto já adicionado";
+                        
+                        
                         carrinhoItem.appendChild(imgCarrinho);
                         carrinhoItem.appendChild(h2Carrinho);
                         carrinhoItem.appendChild(h2CarrinhoPrice);
@@ -114,10 +257,12 @@ function buscarProdutos() {
                 }); 
 
                 excluir.addEventListener("click", function () {
-                    carrinhoDiv.removeChild(imgCarrinho);
-                    carrinhoDiv.removeChild(excluir);
-                    carrinhoDiv.removeChild(h2Carrinho);
-                    carrinhoDiv.appendChild(h2CarrinhoPrice);
+                        carrinhoItem.removeChild(imgCarrinho);
+                        carrinhoItem.removeChild(h2Carrinho);
+                        carrinhoItem.removeChild(h2CarrinhoPrice);
+                        carrinhoItem.removeChild(excluir);
+                       
+                        carrinhoDiv.removeChild(carrinhoItem);
                 });
 
                 
@@ -127,101 +272,6 @@ function buscarProdutos() {
                 mainDiv.appendChild(h2);    
                 mainDiv.appendChild(h2Price);
                 
-            }
-        }
-    }
-}
-
-
-function categorias(botao) {
-    let url = "https://fakestoreapi.com/products/category/" + botao.value;
-        
-        
-    let request = new XMLHttpRequest();
-    request.open("GET", url);
-    request.send();
-
-    request.onload = function () {
-        mainDiv.innerHTML = '';
-        
-        if (request.status === 200) {
-            
-            let produtosEletronicos = JSON.parse(request.response);
-
-
-            for (let index = 0; index < produtosEletronicos.length; index++) {
-                let img = document.createElement("img");
-                img.id = "imagensAPI";
-                img.src = produtosEletronicos[index].image;
-                let h2Modal = document.createElement("h2");
-                
-                h2Modal.innerHTML = "";
-                
-                let excluir = document.createElement("img");
-                excluir.src = "imgs/excluir.png";
-                excluir.id = "excluirImg";
-
-                let h2 = document.createElement("h2");
-                h2.textContent = produtosEletronicos[index].title;
-                h2.id = "nomeProduto";
-                
-
-                let imgCarrinho = document.createElement("img");
-                imgCarrinho.id = "imgCarrinhoAPI";
-                imgCarrinho.src = produtosEletronicos[index].image;
-
-                let h2Carrinho = document.createElement("h2");
-                h2Carrinho.textContent = produtosEletronicos[index].title;
-                h2Carrinho.id = "nomeProduto";
-
-                let h2CarrinhoPrice = document.createElement("h2");
-                h2CarrinhoPrice.textContent ="Preço: $" + produtosEletronicos[index].price;
-
-                let h2Price = document.createElement("h2");
-                h2Price.textContent ="Preço: $" + produtosEletronicos[index].price;
-
-                
-                img.addEventListener("click",function (){
-                    modal.style.display = "block";
-                    h2Modal.textContent = produtosEletronicos[index].description;
-                    modalConteudo.style.animation = "modalAnim 0.5s";
-                    modalConteudo.appendChild(h2Modal);
-                    
-                    modalConteudo.appendChild(carrinhoAdd);
-                    img.style.opacity = 0.7;
-
-                    carrinhoAdd.addEventListener("click", function colocarCarrinho() {
-                        let div = createElement('img')
-                        carrinhoDiv.appendChild(div);
-                        div.src = produtosEletronicos[index].image;
-                        carrinhoDiv.appendChild(excluir);
-                        carrinhoDiv.appendChild(h2Carrinho);
-                        carrinhoDiv.appendChild(h2CarrinhoPrice);
-
-                    });
-                });
-
-                spanFechar.addEventListener("click",function (){
-                    modal.style.display = "none";
-                    modalConteudo.removeChild(h2Modal);
-                    img.style.opacity = 1;
-                });
-
-                excluir.addEventListener("click", function () {
-                    carrinhoDiv.removeChild(imgCarrinho);
-                    carrinhoDiv.removeChild(excluir);
-                    carrinhoDiv.removeChild(h2Carrinho);
-                    
-                });
-
-                
-
-               
-
-                
-                mainDiv.appendChild(img);
-                mainDiv.appendChild(h2);   
-                mainDiv.appendChild(h2Price); 
             }
         }
     }
