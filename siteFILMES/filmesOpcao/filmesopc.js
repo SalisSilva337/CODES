8c514aefde89e9ce6526d7ed01aea088625812a2
Salis Silva <salis.silva@aluno.senai.br>
2024-05-07 22:04:03 -0300

    aaaaaaa

--- siteFILMES/filmesOpcao/filmesopc.js
+++ siteFILMES/filmesOpcao/filmesopc.js
@@ -50,13 +50,13 @@ botaoVerMais.id = "botaoVerMais"
 
 let contador = 0
 
-window.onload = filmes();
+window.onload = todosFilmes();
 
 function home() {
     window.location.href = "../filmes.html";
 }
 
-function filmes() {
+function todosFilmes() {
     contador++
     let url = "https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=" + contador;
 
@@ -68,7 +68,6 @@ function filmes() {
     console.log(url);
 
     let todaLista = JSON.parse(request.response);
-    // let tiposFilmes = todaLista.filter((item) => item.category.name == botao.textContent);
     console.log(todaLista);
 
     for (let index = 0; index < todaLista.results.length; index++) {
@@ -101,9 +100,87 @@ function filmes() {
         divFilmesSeries.appendChild(botaoVerMais);
 
         botaoVerMais.addEventListener("click", function () {
-           filmes(); 
+           todosFilmes(); 
         });
     }
+
+    // let urlGeneros = "https://api.themoviedb.org/3/genre/movie/list?language=pt-BR&page=";
+
+    // let requestGeneros = new XMLHttpRequest();
+    // requestGeneros.open("GET", urlGeneros, false);
+    // requestGeneros.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGZmNWNjMGU0NDhkZDI0ODA2MTRkYjEwNTIyMjcyMCIsInN1YiI6IjY2Mzk1ZGQxNDcwZWFkMDEyYTEzOTdhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v446Yj5-PLMOF25bAjvHOf4VNkrB2gP1n6Oimq26wpE');
+
+    // requestGeneros.send();
+    // console.log(urlGeneros);
+
+    // let todosGeneros = JSON.parse(requestGeneros.response);
+    
+    // console.log(todosGeneros);
+    
 }
 
+
+
+function filmes(botao) {
+    
+    let url = "https://api.themoviedb.org/3/discover/movie?language=pt-BR&with_genres=" + botao.value;
+
+    let request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGZmNWNjMGU0NDhkZDI0ODA2MTRkYjEwNTIyMjcyMCIsInN1YiI6IjY2Mzk1ZGQxNDcwZWFkMDEyYTEzOTdhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v446Yj5-PLMOF25bAjvHOf4VNkrB2gP1n6Oimq26wpE');
+
+    request.send();
+    console.log(url);
+
+    let todaLista = JSON.parse(request.response);
+    console.log(todaLista);
+
+    for (let index = 0; index < todaLista.length; index++) {
+        let urlItens = "https://media.themoviedb.org/t/p/w200";
+
+        let divCadaItem = document.createElement("div");
+        let divConjunto = document.createElement("div");
+        let nomeItem = document.createElement("h2");
+        let imgItem = document.createElement("img");
+        
+        let divImg = document.createElement("div");
+        let descItem = document.createElement("h2");
+        
+        divCadaItem.id = "divCadaItem";
+        imgItem.id = "imgItem";
+        descItem.id = "descItem";
+        divImg.id = "divImg";
+        divConjunto.id = "divConjunto";
+
+        imgItem.src = urlItens + tiposFilmes.results[index].poster_path;
+        nomeItem.textContent = tiposFilmes.results[index].title;
+        descItem.textContent = tiposFilmes.results[index].overview.slice(0,200) + "...";
+        
+        
+        divImg.appendChild(imgItem);
+
+        divCadaItem.appendChild(divImg);
+        divCadaItem.appendChild(nomeItem);
+        divFilmesSeries.appendChild(divCadaItem);
+        divFilmesSeries.appendChild(botaoVerMais);
+
+        botaoVerMais.addEventListener("click", function () {
+           todosFilmes(); 
+        });
+    }
+
+    // let urlGeneros = "https://api.themoviedb.org/3/genre/movie/list?language=pt-BR&page=";
+
+    // let requestGeneros = new XMLHttpRequest();
+    // requestGeneros.open("GET", urlGeneros, false);
+    // requestGeneros.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGZmNWNjMGU0NDhkZDI0ODA2MTRkYjEwNTIyMjcyMCIsInN1YiI6IjY2Mzk1ZGQxNDcwZWFkMDEyYTEzOTdhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v446Yj5-PLMOF25bAjvHOf4VNkrB2gP1n6Oimq26wpE');
+
+    // requestGeneros.send();
+    // console.log(urlGeneros);
+
+    // let todosGeneros = JSON.parse(requestGeneros.response);
+    
+    // console.log(todosGeneros);
+    
+}
 
