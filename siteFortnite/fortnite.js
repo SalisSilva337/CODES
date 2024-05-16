ed6be508144f143c377bfed2e4817c5d77a30008
Salis Silva <salis.silva@aluno.senai.br>
2024-05-16 18:09:29 -0300

    aaaaaaaaa

--- siteFortnite/fortnite.js
+++ siteFortnite/fortnite.js
@@ -15,14 +15,27 @@ function cosmeticos() {
     let listaTrajes = JSON.parse(request.response).data
     console.log(listaTrajes);
 
-    for (let index = 0; index < listaTrajes.length; index++) {
+    for (let index = 0; index < 1000; index++) {
         let skinImg = document.createElement("img");
         let skinName = document.createElement("h1");
         let skinDesc = document.createElement("h2");
         let divCadaItem = document.createElement('div');
 
         divCadaItem.className = "divCadaItem";
+        if (listaTrajes[index].name === "PNJ") {
+            index++
+        }
+        if (listaTrajes[index].name.startsWith("TBD")) {
+            index++
+        }
+        if (listaTrajes[index].id.startsWith("CID_NPC")) {
+            index++
+        }
+        if (listaTrajes[index].description.startsWith("Set_")) {
+            index++
+        }
         
+
         skinImg.src = listaTrajes[index].images.smallIcon;
         divCadaItem.appendChild(skinImg)
         mostrarItem.appendChild(divCadaItem)
